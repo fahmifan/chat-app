25b1e4c05c9b599dba9e8441d06d1f00e963bf6c
miun173 <fahmifan173@gmail.com>
2018-12-29 08:16:19 +0700

    add main theme

---
+++ src/theme/main.js
@@ -0,0 +1,12 @@
+export const theme = { 
+  color: {
+    primary: '#9C27B0',
+    primaryDark: '#7B1FA2',
+    primaryLight: '#E1BEE7',
+    primaryText: '#212121',
+    secondaryText: '#757575',
+    divider: '#BDBDBD',
+    textIcon: '#FFFFFF',
+    accent: '#E040FB',
+  },
+}
